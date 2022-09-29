53dd16cd90f8d48f1400ab60fd26f3182ca18846
Ahmed Attallah <ahmedattallah19010@gmail.com>
2022-09-29 16:30:16 +0200

    [UPDATED] base URL

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: 8000,
   MONGO_URI: "mongodb://localhost/emplopedia",
   JWT_KEY: "1b23d1cf-5dc5-408f-88f3-d98206ff83af",
-  BASE_URL : "http://localhost:8000"
+  BASE_URL : "https://api.emplopedia.com"
 };
